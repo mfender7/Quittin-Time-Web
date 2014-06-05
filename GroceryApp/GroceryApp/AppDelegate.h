83ea83d21dd18e029e082d99603c0331f0a45bee
Shane Owens <shane.owens@gtri.gatech.edu>
2014-06-04 20:58:33 -0400

    Still trying to figure git out

---
+++ GroceryApp/GroceryApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GroceryApp
+//
+//  Created by Shane Owens on 6/4/14.
+//  Copyright (c) 2014 Shane Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GroceryApp/GroceryApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GroceryApp
+//
+//  Created by Shane Owens on 6/4/14.
+//  Copyright (c) 2014 Shane Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
